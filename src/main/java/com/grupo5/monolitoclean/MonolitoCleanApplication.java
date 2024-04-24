d242c31dbcd7257fbabdab90211064a4c95956ab
gabrielteiga <gabrielteiga99@hotmail.com>
2024-04-24 19:42:09 -0300

    Projeto inicial

---
+++ src/main/java/com/grupo5/monolitoclean/MonolitoCleanApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.monolitoclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitoCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolitoCleanApplication.class, args);
+	}
+
+}
